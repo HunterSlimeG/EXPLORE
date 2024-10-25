f8c64ff1aad79a780f697c22202e8c6d9701664a
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-10-25 07:38:18 -0400

    Add files via upload

---
+++ EXPLORE (FBLA)/FBLA 0.16/Scripts/Interaction/coin.gd
@@ -0,0 +1,25 @@
+extends AnimatedSprite2D
+
+var player: CharacterBody2D
+@export var chest = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play("default")
+	player = Globals.player
+	if chest:
+		position.y = player.position.y
+		position.x = player.position.x-200
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func coin():
+	visible = false
+	$Hitbox/CollisionShape2D.disabled = true
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$Hitbox.value = 0
