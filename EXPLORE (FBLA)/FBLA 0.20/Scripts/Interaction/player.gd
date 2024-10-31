2ac9d30ee7c7f842af71ce697125e834d4da9552
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-10-31 07:46:54 -0400

    Add files via upload

---
+++ EXPLORE (FBLA)/FBLA 0.20/Scripts/Interaction/player.gd
@@ -0,0 +1,90 @@
+extends CharacterBody2D
+
+
+var SPEED = Globals.speed
+var health = 100
+var score = 0
+var save_path = Data.score_path
+var data = {"high":0, "current":0}
+var highScore = 0
+var direction: Vector2
+var moveable = false
+var dead = false
+signal scoreSignal()
+
+func _ready() -> void:
+	animation("spawn", true)
+	Globals.player = self
+	data = Data.load_data(save_path)
+	highScore = data.high
+	
+func _physics_process(delta: float) -> void:
+	if moveable:
+		direction.y = Input.get_axis("Up", "Down")
+		direction.x = Input.get_axis("Left", "Right")
+		if direction.y:
+			velocity.y = direction.y * SPEED
+		else:
+			velocity.y = move_toward(velocity.x, 0, SPEED)
+		if direction.x:
+			velocity.x = direction.x * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		direction.x = abs(velocity.x)/velocity.x
+		move_and_slide()
+	else:
+		pass
+
+func animation(anim: String, play: bool):
+	if !anim == "" && play == false:
+		$AnimatedSprite2D.animation = anim
+	elif !anim == "" && play:
+		$AnimatedSprite2D.play(anim)
+	else:
+		pass
+func _process(delta: float) -> void:
+	if health <= 0 && !dead:
+		dead = true
+		death()
+	if direction.x <= 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction.x >= 0:
+		$AnimatedSprite2D.flip_h = false
+		
+func damage(dmg) -> void:
+	health -= dmg
+	$HUD/HealthBar.value = health
+	
+func heal(val) -> void:
+	health += val
+	if health > 100:
+		health = 100
+	$HUD/HealthBar.value = health
+	
+func scoreChange(val) -> void:
+	score += val
+	$HUD/Score.text = "Score: "+str(score)
+	data.current = score
+	emit_signal("scoreSignal")
+
+func death() -> void:
+	moveable = false
+	animation("death", true)
+	$Timer.start()
+
+func win():
+	if score > highScore:
+		highScore = score
+	data.current = score
+	data.high = highScore
+	Data.save_data(data, save_path)
+
+
+func _on_timer_timeout() -> void:
+	data.current = score
+	Data.save_data(data, save_path)
+	get_tree().change_scene_to_file("res://Scenes/Screens/Death.tscn")
+
+
+func _on_timer_2_timeout() -> void:
+	moveable = true
---
+++ EXPLORE (FBLA)/FBLA 0.20/cursor.gd
@@ -0,0 +1,41 @@
+extends Node2D
+var SPEED = 1000.0
+var animation = "default"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = get_global_mouse_position()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Interact"):
+		$Cursor.play(animation)
+	if event is InputEventMouseMotion:
+		if Input.get_current_cursor_shape() == Input.CURSOR_POINTING_HAND:
+			pointer()
+		if Input.get_current_cursor_shape() == Input.CURSOR_ARROW:
+			arrow()
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2
+	direction.y = Input.get_axis("Up2", "Down2")
+	direction.x = Input.get_axis("Left2", "Right2")
+	var movement = SPEED * direction * delta
+	if (movement):
+		get_viewport().warp_mouse(get_global_mouse_position()+movement)
+
+func pointer():
+	animation = "hand"
+	$Cursor.animation = "hand"
+
+func arrow():
+	animation = "default"
+	$Cursor.animation = "default"
+	
+func click():
+	var JoyClick = InputEventMouseButton.new()
+	JoyClick.button_index = MOUSE_BUTTON_LEFT
+	JoyClick.pressed = true
+	
