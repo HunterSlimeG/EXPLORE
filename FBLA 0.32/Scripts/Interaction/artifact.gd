0d0701c8965b61e4c0b201d2346c7405fa814de4
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-11-18 21:35:26 -0500

    Add files via upload

---
+++ FBLA 0.32/Scripts/Interaction/artifact.gd
@@ -0,0 +1,38 @@
+extends AnimatedSprite2D
+
+var player: CharacterBody2D
+@export var chest = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = Globals.player
+	if chest:
+		position.y = player.position.y
+		position.x = player.position.x-200
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func score():
+	$Hitbox/CollisionShape2D.disabled = true
+	$Hitbox2/CollisionShape2D.disabled = true
+	play("default")
+	$AnimationPlayer.play("new_animation")
+	$Timer.start()
+	$Timer2.start()
+	
+func _on_timer_timeout() -> void:
+	$Hitbox.value = 0
+	$Hitbox2.value = 0
+	
+
+
+func _on_timer_2_timeout() -> void:
+	visible = false
+	queue_free()
+func menu():
+	play("default")
+	$AnimationPlayer.play("new_animation")
