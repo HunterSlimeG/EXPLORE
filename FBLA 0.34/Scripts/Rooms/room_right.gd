a8fa3f4487808552e2e0c17311031cd44f263e43
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-11-19 21:38:41 -0500

    Add files via upload

---
+++ FBLA 0.34/Scripts/Rooms/room_right.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var spawn = false
+var roomTurn = preload("res://Scenes/Rooms/Room_Turn.tscn").instantiate()
+var roomChest = preload("res://Scenes/Rooms/Room_Chest.tscn").instantiate()
+var roomT = preload("res://Scenes/Rooms/Room_T.tscn").instantiate()
+var rooms = [roomChest, roomTurn, roomT]
+var room 
+
+# Called when the node enters the scene tree for the first time.
+signal roomSend(r, rs)
+func _ready() -> void:
+	room = rooms.pick_random()
+	roomSend.emit(room, rooms)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	if spawn:
+		roomTurn.scale.x *= -1
+		roomT.scale.x *= -1
+		roomChest.scale.x *= -1
+		rooms = [roomChest, roomTurn, roomT]
+		print(str(room)+"--"+str(self))
+		add_child(room)
